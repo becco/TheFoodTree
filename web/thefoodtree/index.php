af31bd0a3982063300896fabbf77ac84a2570984
Rebecca Richards <rebecca.richards@iqcontent.com>
2015-01-17 01:11:10 +0000

    Some little tidy ups to the web app

--- web/thefoodtree/index.php
+++ web/thefoodtree/index.php
@@ -1,4 +1,4 @@
-<h1>Throw Control Panel</h1>
+<h1>Control Panel</h1>
 <?php
 include('config.php');
 include('db.class.php');
@@ -16,7 +16,7 @@ if(isset($_SESSION['username'])) {
 	$result = $db->query("SELECT * FROM user WHERE email = '$email'");
 	$i = 0;
 	?>
-	<<table>
+	<table>
 		<thead>
 			<tr>
 				<th>ID</th>
@@ -38,13 +38,7 @@ if(isset($_SESSION['username'])) {
 				$db2->query("SELECT * FROM items");
 				
 				while ($line2 = $db2->fetchNextObject()){
-					// $doc_id = $line2->document_id;
 					
-					// $db3 = new DB($database, $host, $user, $password);
-					// $db3->query("SELECT * FROM documents WHERE id = $doc_id");
-					// while ($line3 = $db3->fetchNextObject()){
-					// 	echo '<img src="uploads/'.$line3->name.'" width="205px"/>';
-					// }
 
 					?>
 					<tr>
@@ -65,25 +59,16 @@ if(isset($_SESSION['username'])) {
 	
 	
 	?>
-		
+
 		</tbody>
 	</table>
-<form action="list.php" method="post">
-    <input type="hidden" value="<?php echo $email?>" name="username"/>
-    <input type="submit" value="Get List"/>
-</form>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-	<input type="file" name="upload_file"/>
-    <input type="hidden" value="<?php echo $email?>" name="username"/>
-    <input type="submit" value="Add File"/>
-</form>
 <?php }
 
 else {
 	?>
     <form action="index.php" method="post">
-        <input type="text" value="email address you registered with" name="username"/>
+        <input type="text" value="" name="username"/>
         <input type="submit" value="Log In"/>
     </form>
 <?php 
